654944e54bc815d2fc247c1666c49f9fe9f87f80
Leonardo Vilarinho <leonardo-s@outlook.com>
2016-07-23 10:40:35 -0300

    Comentários

--- kernel/Saved.php
+++ kernel/Saved.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * Class Saved
+ *
  * Created by PhpStorm.
  * User: Leonardo Vilarinho
  * Date: 13/07/2016
@@ -8,17 +10,33 @@
  */
 abstract class Saved
 {
+    /**
+     * Cria um novo salvamento de dados usando dados do usuário para setar o nome da variavel
+     * e aramazenando o array de post atual
+     */
     public static function create()
     {
         Session::set(md5('saved' . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']), $_POST);
     }
 
+    /**
+     * Armazena um array personalizado na variavel de salvamento, para caso não esteja armazenando o post.
+     *
+     * @param $array - lista de dados a ser gravados
+     */
     public static function set($array)
     {
         if(is_array($array))
          Session::set(md5('saved' . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']), $array);
     }
 
+    /**
+     * Pega uma variavel salva no salvamento atual, quando passado a chave, busca no array salvo esse
+     * mesmo elemento, se houver encontrado retorna seu valor, se não retorna uma string vazia.
+     *
+     * @param $key - chave do array, ou seja, nome da variavel
+     * @return string - valor do item encontrado ou string vazia
+     */
     public static function get($key)
     {
         if(isset(Session::get(md5('saved' . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT']))[$key]))
@@ -27,6 +45,9 @@ abstract class Saved
             return '';
     }
 
+    /**
+     * Apaga salvamento atual usando unset.
+     */
     public static function destroy()
     {
         unset($_SESSION[md5('saved' . $_SERVER['REMOTE_ADDR'] . $_SERVER['HTTP_USER_AGENT'])]);
