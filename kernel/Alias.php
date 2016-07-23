acd049e0ef0fd4f33c1554d4d3017a03248b87dd
Leonardo Vilarinho <leonardo-s@outlook.com>
2016-07-23 10:21:22 -0300

    Comentários

--- kernel/Alias.php
+++ kernel/Alias.php
@@ -1,11 +1,14 @@
 <?php
 
 /**
+ * Class Alias
+ *
  * Created by PhpStorm.
  * User: Leonardo Vilarinho
  * Date: 11/07/2016
  * Time: 19:58
  */
+
 abstract class Alias
 {
     /**
@@ -14,12 +17,26 @@ abstract class Alias
     private static $alias = array();
 
 
+    /**
+     * Cria um novo Alias (atalho, ou sinonimo) para uma rota.
+     *
+     * @param $controller - controlador a ser chamado
+     * @param $method - metodo do controlador a ser executado
+     * @param $alias - atalho para URL
+     */
     public static function set($controller, $method, $alias)
     {
         $link = mb_strtolower($controller.'/'.$method);
         self::$alias[$link] = $alias;
     }
 
+    /**
+     * Verifica se um alias existe, se sim retorna controlador/metodo para que ele possa ser traduzido, se
+     * não retorna false.
+     *
+     * @param $alias - nome do atalho
+     * @return mixed - link da rota ou false
+     */
     public static function check($alias)
     {
         $alias = mb_strtolower($alias);
@@ -29,6 +46,13 @@ abstract class Alias
             return false;
     }
 
+    /**
+     * Pega um alias quando passado um controlador e um metodo.
+     *
+     * @param $controller - controlador do alias
+     * @param $method - metodo do alias
+     * @return bool|mixed - string com nome do alias ou false
+     */
     public static function get($controller, $method)
     {
         $link = mb_strtolower($controller.'/'.$method);
